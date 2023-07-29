d9017dc3a6fd04a055594084b7a59f347c214ba1
Romeo Macline D'Costa <119812291+Ultimatum954@users.noreply.github.com>
2023-07-29 12:22:20 +0600

    Update allMarks.m

--- allMarks.m
+++ allMarks.m
@@ -3,3 +3,5 @@ allMarks=[24 44 36;52 57 68;66 53 69;85 40 86;15 47 25;79 72 82]  % executes all
 mechanics = [36;76;73;72;28;91] % creates a new column named mechanics with 6 rows
 
 allMarks= [allMarks,mechanics] % takes the mechanics column and combines it with the allMarks matrix creating the new allMarks matrix which is 6 by 4 now
+
+allMarks(:,4)=[]  % removes the mechanics column
