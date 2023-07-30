42b7a8f1f0a46f6ec5f0cab431b0dffebb20d5a5
Romeo Macline D'Costa <119812291+Ultimatum954@users.noreply.github.com>
2023-07-30 14:50:57 +0600

    Update Task_6.m

--- Task_6.m
+++ Task_6.m
@@ -1,5 +1,18 @@
+%code 1
+
 function res(n)
   if (n<10)
+    disp("Fail");
+  else
+    disp("Pass");
+  endif
+endfunction
+
+%code 2
+
+function a=res(x,y,z)
+  a=x+y+z;
+  if (a<10)
     disp("Fail");
   else
     disp("Pass");
